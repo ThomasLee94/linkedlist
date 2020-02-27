3dcf6fb4bb6a199ecc40a51cd4c0d636408bc05d
Thomas Lee <thomaslee9474@gmail.com>
2020-02-27 03:08:06 +0000

    [WIP] testing functionality

--- linkedlist/main.cc
+++ linkedlist/main.cc
@@ -1,3 +1,34 @@
-#include "node.h"
 #include "linkedlist.h"
-#include "linkedlist.cc"
+
+#include <iostream>
+#include <vector>
+
+namespace linkedlist {
+    
+int main(int argc, char** argv) {
+  std::vector<int> nums({1, 2, 3, 4, 5});
+
+  LinkedList linked_list_obj(nums);
+
+  std::cout << "----- before deletion -----" << std::endl;
+
+  Node* node = linked_list_obj.head;
+  for (int i=0; i < linked_list_obj.size; i++) {
+    std::cout << node->data << std::endl;
+    node = node->next;
+  }
+
+  linked_list_obj.delete_(4);
+
+  std::cout << "***** after deletion *****" << std::endl;
+
+  node = linked_list_obj.head;
+  for (int i=0; i < linked_list_obj.size; i++) {
+    std::cout << node->data << std::endl;
+    node = node->next;
+  }
+
+  return 0;
+}
+
+} // namespace linkedlist
